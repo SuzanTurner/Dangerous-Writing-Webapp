a19e63b42f09e2eafa1b7bcc993001186b0bdd6c
Yadh <yadhnikawakde@gmail.com>
2025-03-08 10:47:59 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/write')
+def write():
+    return render_template('write.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
